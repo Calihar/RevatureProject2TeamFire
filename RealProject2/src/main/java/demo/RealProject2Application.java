62908846d3a0833fcbca23a160c0227a3604c841
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-05 12:40:52 -0400

    Spring Starter Project now

---
+++ RealProject2/src/main/java/demo/RealProject2Application.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealProject2Application.class, args);
+	}
+
+}

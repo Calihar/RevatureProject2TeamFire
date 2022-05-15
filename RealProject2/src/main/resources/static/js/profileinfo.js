725ac42f2dccf461967133ccc7c33286f4bad7e8
Hiroyuki0487 <Hiroyuki0487@users.noreply.github.com>
2022-05-15 15:24:57 -0400

    Profile Page Modal complete

--- RealProject2/src/main/resources/static/js/profileinfo.js
+++ RealProject2/src/main/resources/static/js/profileinfo.js
@@ -8,12 +8,6 @@ window.onload = function () {
     document.getElementById("modalUpdateBtn").addEventListener('click', updateUserInfo);
 }
 
-
-//document.quaryselector("#").value;
-
-/*Document.quaryselectort("#idstring").value;
-html how to get user input json via js*/
-
 function changeEmailFunction() {
     console.log("In Change Email Function");
     let emailParent = document.querySelector("#emailParent");
@@ -22,7 +16,7 @@ function changeEmailFunction() {
     emailChild = document.createElement("input");
     emailChild.setAttribute("id", "emailChild");
     emailChild.setAttribute("type", "email");
-    emailChild.setAttribute("value", "New Email Address");
+    emailChild.setAttribute("placeholder", "New Email Address");
     emailChild.setAttribute("name", "childEmailValue");
     emailChild.innerText = "(Filled From DB)";
     emailParent.appendChild(emailChild);
@@ -36,7 +30,7 @@ function changeBirthDayFunction() {
     birthDayChild = document.createElement("input");
     birthDayChild.setAttribute("id", "birthDayChild");
     birthDayChild.setAttribute("type", "date");
-    birthDayChild.setAttribute("value", "New Birthdate");
+    birthDayChild.setAttribute("placeholder", "New Birthdate");
     birthDayChild.setAttribute("name", "birthDayValue");
     birthDayChild.innerText = "(Filled From DB)";
     birthDayParent.appendChild(birthDayChild);
@@ -50,7 +44,7 @@ function changeFirstNameFunction() {
     firstNameChild = document.createElement("input");
     firstNameChild.setAttribute("id", "firstNameChild");
     firstNameChild.setAttribute("type", "text");
-    firstNameChild.setAttribute("value", "New First Name");
+    firstNameChild.setAttribute("placeholder", "New First Name");
     firstNameChild.setAttribute("name", "firstNameValue");
     firstNameChild.innerText = "(Filled From DB)";
     firstNameParent.appendChild(firstNameChild);
@@ -64,7 +58,7 @@ function changeLastNameFunction() {
     lastNameChild = document.createElement("input");
     lastNameChild.setAttribute("id", "lastNameChild");
     lastNameChild.setAttribute("type", "text");
-    lastNameChild.setAttribute("value", "New Last Name");
+    lastNameChild.setAttribute("placeholder", "New Last Name");
     lastNameChild.setAttribute("name", "lastNameValue");
     lastNameChild.innerText = "(Filled From DB)";
     lastNameParent.appendChild(lastNameChild);
@@ -75,24 +69,40 @@ function updateUserInfo() {
     let childEmailValue = document.querySelector("#emailChild").value;
     let firstNameValue = document.querySelector("#firstNameChild").value;
     let lastNameValue = document.querySelector("#lastNameChild").value;
-    let birthDayValue = document.querySelector("#birthDayChild").value;
+    let userBirthday = document.querySelector("#birthDayChild").value;
     let userBiographyTextAreaValue = document.querySelector("#userBiographyTextArea").value;
 
+    let userObject={ 
+        "userEmail": childEmailValue,
+        "firstName": firstNameValue,
+        "lastName":lastNameValue,
+        "userBirthday":userBirthday,
+        "userBio":userBiographyTextAreaValue
+    };
+    console.log(userObject);
+    
+    
+    let xhttp = new XMLHttpRequest();
+    
+    
+    
+    xhttp.onreadystatechange = function () {
+        console.log("readyState is changing: ", xhttp.readyState);
 
-    console.log("-----TESTING INPUT BLOCK-----" + "\n Email: " + childEmailValue + "\n" + " First Name: " + firstNameValue + "\n Last name: " + lastNameValue + "\n Birthday: " + birthDayValue + "\n Biography Info: \n" + userBiographyTextAreaValue + "\n-----END OF TESTING INPUT BLOCK-----");
-
-
-    console.log("-----TESTING JSON BLOCK-----")
-    let Json = JSON.stringify({ childEmailValue, firstNameValue, lastNameValue, birthDayValue, userBiographyTextAreaValue });
-    console.log(Json);
-    console.log("-----END OF JSON TESTING BLOCK-----")
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log("readyState is 4!!! AND status is 200!!!");
+        }
+    }
 
 
 
 
-
-
-
+    xhttp.open('POST', `http://localhost:9001/profile/update`);
+    
+    xhttp.setRequestHeader("content-type", "application/json");
+    
+    //xhttp.send(JSON.stringify(userObject));
+    
 }
 
 function getUserInfo(respObj) {
@@ -142,7 +152,6 @@ function getUserInfo(respObj) {
     lastNameChild.innerText = "(Filled From DB)";
     lastNameParent.appendChild(lastNameChild);
 }
-
 
 
 function startUp() {

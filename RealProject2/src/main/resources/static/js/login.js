3af78c6996ad0949b17b00695dca85a20be82dbe
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-15 00:18:37 -0400

    Caleb forgot about String.equals();

--- RealProject2/src/main/resources/static/js/login.js
+++ RealProject2/src/main/resources/static/js/login.js
@@ -1,13 +1,13 @@
 window.onload = function () {
-    
+
     document.getElementById('login').addEventListener("click", loginCheck);
 
 }
 
 function preventBack() {
-    window.history.forward(); 
+    window.history.forward();
 }
-  
+
 setTimeout("preventBack()", 0);
 
 window.onunload = () => {
@@ -17,78 +17,68 @@ window.onunload = () => {
 
 
 
-function noEmptyFields() {
-    let userName = document.getElementById("username").value;
-        console.log("username: "+ userName.value);
-        console.log("username: "+ userName);
-        console.log("username: "+ typeof userName);
-    let passWord = document.getElementById("password").value;
+function noEmptyFields(userName, passWord) {
 
     if (userName == "" || passWord == "") {
         return false;
-    } else if (userName != "" & passWord != ""){
-        return true;
     } else {
-        return false;
+        return true;
     }
 }
 
-function loginCheck() {   
-    
-    console.log(noEmptyFields());
-    if (noEmptyFields()) {
-        userLogin();        
+function loginCheck() {
+
+    var userName = document.querySelector("#username").value;
+    var passWord = document.querySelector("#password").value;
+
+    console.log(noEmptyFields(userName, passWord));
+    if (noEmptyFields(userName, passWord)) {
+        userLogin(userName, passWord);
     } else {
         document.getElementById('texto').innerHTML = "All fields must be filled";
 
-    }  
+    }
 }
 
 function userLogin(userName, passWord) {
-    
+
     let xhttp = new XMLHttpRequest;
 
     xhttp.open('POST', `http://localhost:9001/l-authentication`);
     xhttp.setRequestHeader("content-type", "application/json");
 
-    xhttp.onreadystatechange = function(){
-        if (xhttp.readyState == 4 && xhttp.status== 200) {            
-            
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
             // let regObj = xhttp.responseText;
             // console.log(regObj);
 
-            if(xhttp != null && xhttp != undefined) {
+            ///use this if the response text is the URI for user home page
 
-                ///use this if the response text is the URI for user home page
-                console.log("I'm in!")
-                window.location = xhttp.responseText;
+            console.log("I'm in!")
+            var urlBase = "http://localhost:9001"
+            window.location = urlBase + xhttp.responseText;
 
-                ///this if response text only says valid or invalid
-                //maybe go with the above line instead of this one
-                /////////this part should redirect to user home page after successfull login
-                // window.location.replace("../html/logout.html");
+            ///this if response text only says valid or invalid
+            //maybe go with the above line instead of this one
+            /////////this part should redirect to user home page after successfull login
+            // window.location.replace("../html/logout.html");
 
-
-             } else {
-                // document.getElementById('texto').innerHTML = "In the user validation thingy"; 
-                document.getElementById('texto').innerHTML = "Invalid username and/or password"
-             }
-
-            
         }
-    }    
-        
+    }
+    //no field is empty
+    let userValidation = {
+        "username": userName,
+        "password": passWord
+    }
+    console.log(userValidation);
+    
     // & passwordMatching()
-    if (noEmptyFields() ) {
+    if (noEmptyFields()) {
 
-        //no field is empty
-        let userValidation = {            
-            "username" : userName,            
-            "password" : passWord    
-        }
 
         // console.log("Inside the json block");
-        xhttp.send(JSON.stringify(userValidation));        
-    } 
-}
+        xhttp.send(JSON.stringify(userValidation));
+    }
 
+}

4d89ad098f20615e4b465b0423e52272985d1059
mateoer <102526524+mateoer@users.noreply.github.com>
2022-05-16 19:17:10 -0400

    LOGIN/REGISTRATION functional with static hashed password

--- RealProject2/src/main/resources/static/js/registercopy2.js
+++ RealProject2/src/main/resources/static/js/registercopy2.js
@@ -1,4 +1,4 @@
-async function myEncryptionFunc() {
+/* async function myEncryptionFunc() {
 
   const key = await crypto.subtle.generateKey(
     // The algorithm is AES in CBC mode, with a key length
@@ -58,34 +58,74 @@ async function myEncryptionFunc() {
   );
  
   return message;
-};
+}; */
 
 window.onload = function () {
+  // document
+  //   .getElementById("submit")
+  //   .addEventListener("click", myEncryptionFunc);
+  // document.getElementById("submit").addEventListener("click", newUserRegister);
   document
     .getElementById("submit")
-    .addEventListener("click", myEncryptionFunc);
-  document
-    .getElementById("submit")
-    .addEventListener("click", newUserRegister);
-  
+    .addEventListener("click", myFunction);
 };
 
+function fakeMathRandom(callBack) {
+  if (!callBack) throw new Error("Must provide callBack function");
+  let seed = 0;
+  const randomOutputs = [
+    0.04, 0.08, 0.15, 0.16, 0.23, 0.42, 0.52, 0.65, 0.79, 0.89,
+  ];
+  const Math_random = Math.random;
+  Math.random = function () {
+    return randomOutputs[seed++ % 10];
+  };
+  const callbackOutput = callBack();
+  Math.random = Math_random;
+  return callbackOutput;
+}
 
-function newUserRegister() {   
+function myFunction() {
+  var text = "panchovilla";
+  var key = "secret";
+
+  var encrypted = fakeMathRandom(() => CryptoJS.AES.encrypt(text, key)); //This will always return U2FsdGVkX18KPXCjFHrhR4Q5zBbjCf+I/m/w9jbS3EuvE59kzUxK45FrGHDpqalt
+  var encrypted2 = fakeMathRandom(() => CryptoJS.AES.encrypt(text, key));
+  var encrypted3 = fakeMathRandom(() => CryptoJS.AES.encrypt(text, key));
+
+  var decrypted = CryptoJS.AES.decrypt(encrypted, key).toString(
+    CryptoJS.enc.Utf8
+  );
+
+  console.log("encrypted: "+encrypted);
+  console.log("encrypted2: "+encrypted2);
+  console.log("encrypted3: "+encrypted3);
+  console.log("decrypted: "+decrypted);
+  console.log(encrypted.toString());
+  
+}
+
+/* function newUserRegister() {   
 
   // console.log(myEncryptionFunc());
   // var userPasswordField = myEncryptionFunc();  
   // // console.log(myEncryptionFunc());      
   // console.log("userPasswordField: " + userPasswordField);
   
-  // var encrypted = CryptoJS.AES.encrypt(document.querySelector("#password").value, "Secret Passphrase");
-  var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+  var encrypted = CryptoJS.AES.encrypt(document.querySelector("#password").value, "Secret Passphrase");
+  // var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
   var password = encrypted.toString();
-  console.log(password);
+  // console.log(password);
+
+  var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+  var decryptPass = decrypted.toString();
+  // console.log(decryptPass);
+
+  var panchoPass = CryptoJS.AES.decrypt("U2FsdGVkX19jtwN4Txej3Tssi5eAlncnzdbenjUejqw=", "Secret Passphrase");
+  console.log("U2FsdGVkX19jtwN4Txej3Tssi5eAlncnzdbenjUejqw=");
+  console.log("Pancho pass: "+panchoPass.toString());
+  console.log("U2FsdGVkX196Z6JQ3L6Ba/umkFv+EvSotlKz6ukPcTw=");
+  console.log("Pancho pass: "+CryptoJS.AES.decrypt("U2FsdGVkX196Z6JQ3L6Ba/umkFv+EvSotlKz6ukPcTw=", "Secret Passphrase"));
   
   // U2FsdGVkX1+jPpdxATfXaRjpKdQFVJ5A/W2G1HOyYnY=
-}
-
-
-
-
+} */

605f476157292cff06e4817d273c389061e4eb8b
CalebJGulledge <calebjgulledge@gmail.com>
2022-05-17 08:53:02 -0400

    Working?

--- RealProject2/src/main/java/demo/controller/PostController.java
+++ RealProject2/src/main/java/demo/controller/PostController.java
@@ -113,14 +113,13 @@ public class PostController {
 
 	}
 	
-	@PostMapping("/get/postowner/{id}")
-	public UserModel getpostOwner(HttpSession session, @PathVariable("id") int postId) {
+	@PostMapping("/get/postownerpic/{id}")
+	public String getpostOwnerPic(HttpSession session, @PathVariable("id") int postId) {
 		
 		UserModel currentUser = (UserModel) session.getAttribute("loggedUser");
 		if (currentUser != null) {
-			UserModel tempUser = postDao.findUserIdByPostId(postId);
-			System.out.println(tempUser);
-			return tempUser;
+			PostModel tempPost = postDao.findByPostId(postId);
+			return tempPost.getMyOwner().getUsername() + "|" + tempPost.getMyOwner().getProfilePicName();
 		}
 		return null;
 	}
@@ -131,6 +130,18 @@ public class PostController {
 		UserModel currentUser = (UserModel) session.getAttribute("loggedUser");
 		if (currentUser != null) {
 			return postDao.findAll();
+		}
+		return null;
+	}
+	
+	@PostMapping("/getall/userposts/{username}")
+	public List<PostModel> getAllPostsByUser(HttpSession session, @PathVariable("username") String username) {
+		
+		UserModel currentUser = (UserModel) session.getAttribute("loggedUser");
+		if (currentUser != null) {
+			UserModel tempUser = userDao.findByUsername(username);
+			
+			return postDao.findAllByMyOwner(tempUser);
 		}
 		return null;
 	}
